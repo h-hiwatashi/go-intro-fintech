29ec941fb3f05771593ac04ebdfe2f2180d70907
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-10-03 00:53:16 +0900

    スライスの参照渡し、値渡し

--- src/9/main.go
+++ src/9/main.go
@@ -5,11 +5,6 @@ import (
 	"time"
 )
 
-
-func main() {
-	testSlice()
-}
-
 // channel
 //複数のゴルーチン間のデータの受け渡しを行うためのデータ構造
 //チャネルは、データの送受信を行うための機能を提供する
@@ -22,7 +17,7 @@ func testChannel(){
 	// ch1 = make(chan int)
 	// ch2 := make(chan int)
 	ch3 := make(chan int, 5)
-
+	
 	ch3 <- 1;
 	ch3 <- 2;
 	ch3 <- 3;
@@ -44,64 +39,84 @@ func testChannelGoRoutine(){
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 	// go func(){
-	// 	ch <- 1
-	// }()
-	// i := <- ch
-	// fmt.Println(i)
-	go reciever(ch1)
-	go reciever(ch2)
-
-	for i := 0; i < 10; i++{
-		ch1 <- i
-		ch2 <- i
-		time.Sleep(10 * time.Millisecond)
+		// 	ch <- 1
+		// }()
+		// i := <- ch
+		// fmt.Println(i)
+		go reciever(ch1)
+		go reciever(ch2)
+		
+		for i := 0; i < 10; i++{
+			ch1 <- i
+			ch2 <- i
+			time.Sleep(10 * time.Millisecond)
+		}
 	}
-}
-func reciever(ch <- chan int){
-	for {
-		i := <- ch
-		fmt.Println(i)
+	func reciever(ch <- chan int){
+		for {
+			i := <- ch
+			fmt.Println(i)
+		}
 	}
-}
+	
+	// スライスについて
+	// スライスは、配列の一部を参照するためのデータ構造
+	func testSlice(){
+		// var s []int
+		// s = make([]int, 5)
+		s := make([]int, 5)
+		fmt.Println(s)
+		s[0] = 1
+		s[1] = 2
+		s[2] = 3
+		s[3] = 4
+		s[4] = 5
+		fmt.Println(s)
+		fmt.Println(s[0])
+		fmt.Println(s[1])
+		fmt.Println(s[2])
+		fmt.Println(s[3])
+		fmt.Println(s[4])
+		
+		//暗黙的なスライスの宣言
+		s2 := []int{1, 2, 3, 4, 5}
+		fmt.Println(s2)
+		
+		//make()関数を使用してスライスを作成する
+		s3 := make([]int, 5)
+		fmt.Println(s3)
+		
+		//スライスの要素数を取得する
+		fmt.Println(len(s3))
+		
+		//スライスのキャパシティを取得する
+		fmt.Println(cap(s3))
+	}
+	
+	// スライスのコピー
+	func testSliceCopyTest(){
+		s1 := []int{1, 2, 3, 4, 5}
+		s2 := make([]int, 5)
+		copy(s2, s1)
+		fmt.Println(s2)
 
-// スライスについて
-// スライスは、配列の一部を参照するためのデータ構造
-func testSlice(){
-	// var s []int
-	// s = make([]int, 5)
-	s := make([]int, 5)
-	fmt.Println(s)
-	s[0] = 1
-	s[1] = 2
-	s[2] = 3
-	s[3] = 4
-	s[4] = 5
-	fmt.Println(s)
-	fmt.Println(s[0])
-	fmt.Println(s[1])
-	fmt.Println(s[2])
-	fmt.Println(s[3])
-	fmt.Println(s[4])
+		// 参照渡し
+		s3 := []int{1, 2, 3, 4, 5}
+		s4 := s3
+		s4[0] = 100
+		fmt.Println(s3)
 
-	//暗黙的なスライスの宣言
-	s2 := []int{1, 2, 3, 4, 5}
-	fmt.Println(s2)
+		// 値渡し
+		s5 := []int{1, 2, 3, 4, 5}
+		s6 := make([]int, 4)
+		// nはコピーした要素数
+		// 要素より多いと末尾が消される
+		n := copy(s6, s5)
+		s6[0] = 100
+		fmt.Println(n, s5, s6)
+	}
 
-	//make()関数を使用してスライスを作成する
-	s3 := make([]int, 5)
-	fmt.Println(s3)
 
-	//スライスの要素数を取得する
-	fmt.Println(len(s3))
-
-	//スライスのキャパシティを取得する
-	fmt.Println(cap(s3))
-}
-
-// スライスのコピー
-func testSliceCopyTest(){
-	s1 := []int{1, 2, 3, 4, 5}
-	s2 := make([]int, 5)
-	copy(s2, s1)
-	fmt.Println(s2)
-}
+	func main() {
+		testSliceCopyTest()
+	}
