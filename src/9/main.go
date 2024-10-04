1622c9f1d5053d811e1ed1acebc2f134ed9f51c4
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-10-05 00:17:26 +0900

    チャンネル　select

--- src/9/main.go
+++ src/9/main.go
@@ -232,11 +232,74 @@ func testMap(){
 			}
 		}
 
+		//チャネルのselect文
+		func testChannelSelect(){
+			ch1 := make(chan int, 2)
+			ch2 := make(chan string, 2)
+
+			ch2 <- "A"
+			// この状態ではch1にはデータがないのでdeadlockになる
+			v1:= <- ch1
+			v2:= <- ch2
+			fmt.Println(v1)
+			fmt.Println(v2)
+		}
+		func testChannelSelect2(){
+			ch1 := make(chan int, 2)
+			ch2 := make(chan string, 2)
+
+			ch2 <- "A"
+			//select文を使用すると、複数のチャネルのデータを受信することができる
+			//select文は、複数のcase文を持つことができる
+			//select文は、複数のcase文の中から、ランダムに一つを選択して実行する
+			//select文は、デフォルトのcase文を持つことができる
+			//select文は、デフォルトのcase文は、他のcase文が実行できない場合に実行される
+			// この状態ではch1にはデータがないのでdeadlockになる
+			select{
+				//チャネルに対する操作のみを記述できる
+			case v1:= <- ch1:
+				fmt.Println(v1)
+			case v2:= <- ch2:
+				fmt.Println(v2)
+			default:
+				fmt.Println("default")
+			}
+
+			ch3 := make(chan int, 2)
+			ch4 := make(chan int, 2)
+			ch5 := make(chan int, 2)
+			go func() {
+				for {
+					i := <- ch3
+					ch4 <- i * 2
+				}
+			}()
+			go func() {
+				for {
+					i2 := <- ch4
+					ch5 <- i2 -1
+				}
+			}()
+			n := 0
+			for {
+				select{
+				case ch3 <- n:
+					n++
+				case i3 := <- ch5:
+					fmt.Println("recieved", i3)
+				}
+				if n > 100 {
+					break
+				}
+			}
+
+		}
+
 
 func main() {
 	//可変長引数
 	// fmt.Println(testSliceVariable(1,2,3,4,5))
 
 
-	testChannelFor()
+	testChannelSelect2()
 }
