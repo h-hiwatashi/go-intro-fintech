4516d3641dc6316c84b2c823160f87b79762c55e
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-10-06 13:11:12 +0900

    scope

--- src/13/main.go
+++ src/13/main.go
@@ -18,6 +18,24 @@ func TestScope(){
 	Println(foo.ReturnMin())
 }
 
+//関数内で定義された定数は他の関数で呼び出せない
+// sは引数
+// bは名前付きの戻り値
+func TestScope2(s string)(b string){
+	const a = "a"
+	b = a + s
+	{
+		const c = "c"
+		// 深いブロック内でのみ有効
+		b := c + s
+		Println(b)
+	}
+	Println(b)
+	return
+	
+}
+
 func main() {
 	TestScope()
+	TestScope2("AAA")
 }
