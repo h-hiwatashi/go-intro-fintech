6bc742a3a148b18d405f1ffd0eb8292a471f9488
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-10-16 07:38:52 +0900

    criptoパッケージ sha

--- src/16/main.go
+++ src/16/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"flag"
 	"fmt"
 	"io"
@@ -647,9 +650,33 @@ func testCrypto(){
 	h:=md5.New()
 	io.WriteString(h, "test")
 	fmt.Println(h.Sum(nil))
-	
+
 	s:= fmt.Sprintf("%x", h.Sum(nil))
 	fmt.Println(s)
+
+
+	    //------------------------------
+    /* SHA-1 */
+    s1 := sha1.New()
+    io.WriteString(s1, "ABCDE")
+    fmt.Printf("%x\n", s1.Sum(nil))
+    // => f0393febe8baaa55e32f7be2a7cc180bf34e52137d99e056c817a9c07b8f239a
+ 
+    //------------------------------
+ 
+    /* SHA-256 */
+    s256 := sha256.New()
+    io.WriteString(s256, "ABCDE")
+    fmt.Printf("%x\n", s256.Sum(nil))
+    // => f0393febe8baaa55e32f7be2a7cc180bf34e52137d99e056c817a9c07b8f239a
+ 
+    //------------------------------
+ 
+    /* SHA-512 */
+    s512 := sha512.New()
+    io.WriteString(s512, "ABCDE")
+    fmt.Printf("%x\n", s512.Sum(nil))
+	// => 9989a8fcbc29044b5883a0a36c146fe7415b1439e995b4d806ea0af7da9ca4390eb92a604b3ecfa3d75f9911c768fbe2aecc59eff1e48dcaeca1957bdde01dfb
 }
 
 
