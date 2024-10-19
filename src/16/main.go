75aa29406292cdb75c71c15bd458af81710bfeb4
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-10-20 00:11:05 +0900

    net/httpパッケージ クライアント

--- src/16/main.go
+++ src/16/main.go
@@ -15,6 +15,7 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"net/http"
 	"net/url"
 	"os"
 	"regexp"
@@ -895,9 +896,159 @@ func testUrl(){
 	fmt.Println(u2)
 }
 
+// net/httpパッケージ
+// クライアント
+func testHttpClient(){
+	// net/httpパッケージ
+	// HTTPクライアントを作成する
+	// http.Get()
+	// GETリクエストを送信する
+	// http.Post()
+	// POSTリクエストを送信する
+	// http.PostForm()
+	// POSTリクエストを送信する
+	// http.NewRequest()
+	// リクエストを作成する
+	// http.Client
+	// カスタムのHTTPクライアントを作成する
 
+	// GETリクエストを送信する
+	resp, _ := http.Get("https://example.com")
+	fmt.Println(resp.StatusCode)
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	// POSTリクエストを送信する
+	resp, err := http.PostForm("https://example.com", url.Values{"key": {"value"}})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, _ = ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	// リクエストを作成する
+	req, _ := http.NewRequest("GET", "https://example.com", nil)
+	req.Header.Set("Content-Type", "text/plain")
+	client := &http.Client{}
+	resp, _ = client.Do(req)
+	defer resp.Body.Close()
+	body, _ = ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	//応用
+	// //ヘッダーをつけたり、クエリをつけたり
+	// //Parse 正しいURLか確認
+	// base, _ := url.Parse("https://example.com/")
+ 
+	// //クエリ の確認 URLの後につく
+	// reference, _ := url.Parse("index/lists?id=1")
+ 
+	// //ResolveReference
+	// //クエリをくっつけたURLを生成する。
+	// //相対パスから絶対URLに変換する。
+	// //baseのURLの末尾に文字列が入っていたとしても、正しいURLに直してくれる
+	// endpoint := base.ResolveReference(reference).String()
+	// fmt.Println(endpoint)
+ 
+	// //GET ver
+	// //リクエストを作成 nil部はPOST時のみ設定（バイトを入れる）
+	// //まだリクエストはしていない。structを作っただけ。
+	// req, _ := http.NewRequest("GET", endpoint, nil)
+ 
+	// //requestにheaderをつける。cash情報など
+	// req.Header.Add("Content-Type", `application/json"`)
+ 
+	// //URLのクエリを確認
+	// q := req.URL.Query()
+ 
+	// //クエリを追加
+	// q.Add("name", "test")
+ 
+	// //クエリを表示
+	// fmt.Println(q)
+ 
+	// //&など特殊文字などがある場合があるため、encodingしてからURLに追加してやる必要がある
+	// fmt.Println(q.Encode())
+ 
+	// //encodeしてからURLに戻す
+	// //日本語などを変換する
+	// req.URL.RawQuery = q.Encode()
+ 
+	// //実際にアクセスする
+	// //クライアントを作る
+	// var client *http.Client = &http.Client{}
+ 
+	// //結果 アクセスする
+	// resp, _ := client.Do(req)
+ 
+	// //読み込み
+	// body, _ := ioutil.ReadAll(resp.Body)
+ 
+	// //出力
+	// fmt.Println(string(body))
+
+	// Post ver
+	//応用
+    // //ヘッダーをつけたり、クエリをつけたり
+    // //Parse 正しいURLか確認
+    // base, _ := url.Parse("https://example.com/")
+ 
+    // //クエリ の確認 URLの後につく
+    // reference, _ := url.Parse("index/lists?id=1")
+ 
+    // //Postの時のデータ
+    // AccountData := &Account{ID: "ABC-DEF", PassWord: "testtest"}
+    // data, _ := json.Marshal(AccountData)
+ 
+    // //ResolveReference
+    // //クエリをくっつけたURLを生成する。
+    // //相対パスから絶対URLに変換する。
+    // //baseのURLの末尾に文字列が入っていたとしても、正しいURLに直してくれる
+    // endpoint := base.ResolveReference(reference).String()
+    // fmt.Println(endpoint)
+ 
+    // //POST ver
+    // //bytes.NewBuffer([]byte("password")でリクエストの領域を作成
+    // //POSTの場合は、Bodyにデータを入れる。例えばパスワード。見られたらダメな情報はbodyに
+    // req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(data))
+ 
+    // //requestにheaderをつける。cash情報など
+    // req.Header.Add("Content-Type", `application/json"`)
+ 
+    // //URLのクエリを確認
+    // q := req.URL.Query()
+ 
+    // //クエリを追加
+    // q.Add("name", "test")
+ 
+    // //クエリを表示
+    // fmt.Println(q)
+ 
+    // //&など特殊文字などがある場合があるため、encodingしてからURLに追加してやる必要がある
+    // fmt.Println(q.Encode())
+ 
+    // //encodeしてからURLに戻す
+    // //日本語などを変換する
+    // req.URL.RawQuery = q.Encode()
+ 
+    // //実際にアクセスする
+    // //クライアントを作る
+    // var client *http.Client = &http.Client{}
+ 
+    // //結果 アクセスする
+    // resp, _ := client.Do(req)
+ 
+    // //読み込み
+    // body, _ := ioutil.ReadAll(resp.Body)
+ 
+    // //出力
+    // fmt.Println(string(body))
+}
 
 
 func main() {
-	testUrl()
+	testHttpClient()
 }
