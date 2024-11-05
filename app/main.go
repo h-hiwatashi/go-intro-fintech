2fc12ab747498366739ef8d78262d9ca71c44ce5
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-11-05 19:20:00 +0900

    コメント追加

--- app/main.go
+++ app/main.go
@@ -10,15 +10,18 @@ import (
 var Db *sql.DB
 
 func main() {
+	// DB名を指定してDBを開く
+	// 今回はexample.sql
 	Db, _ := sql.Open("sqlite3", "./example.sql")
 
 	defer Db.Close()
-	//バクスラで複数行の文字列を書く
+	//　\で複数行の文字列を書く
 	// もしpersonsテーブルがなければ作成する
 		cmd := `CREATE TABLE IF NOT EXISTS persons(
 			name STRING,
 			age INT)`
 
+	
 	_, err := Db.Exec(cmd)
 	if err != nil {
 		log.Fatalln(err)
