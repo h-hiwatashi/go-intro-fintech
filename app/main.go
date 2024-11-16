7dbd098427c7a4c580c2ecfbc149aebed6528d7f
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-11-16 18:26:35 +0900

    DBの接続を設定

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var Db *sql.DB
-
+var err error
 type Person struct {
 	Name string
 	Age  int
@@ -17,12 +17,19 @@ type Person struct {
 
 func main() {
 	// DB名を指定してDBを開く
-	// 今回はexample.sql
 	//今回は非sslモードで接続
 	// var err error
-	Db, err := sql.Open("postgres", "user=test_user dbname=testdb password=password sslmode=disable")
+	Db, err = sql.Open("postgres", "host=postgres port=5432 user=root password=root dbname=dev sslmode=disable")
+	// Db, err = sql.Open("postgres", "user=root password=root dbname=dev host=localhost port=5432 sslmode=disable")
 	if err != nil{
 		log.Panicln(err)
 	}
 	defer Db.Close()
+
+	//create
+	cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)"
+	_, err := Db.Exec(cmd, "Hanako", 30)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
