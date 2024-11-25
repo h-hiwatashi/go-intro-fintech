8a1b53275c2a0134706302a31522d8417a439edd
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-11-25 23:57:43 +0900

    html templete追加

--- todo_app/app/controllers/route_main.go
+++ todo_app/app/controllers/route_main.go
@@ -1,11 +1,25 @@
 package controllers
 
 import (
+	"html/template"
+	"log"
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request){
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	// ParseFiles関数を使って、テンプレートファイルをパースする
+	// htmlテンプレートファイルをパースし、テンプレートオブジェクトが返される
+	// テンプレートオブジェクトのExecuteメソッドを使って、テンプレートをブラウザにレンダリングする
+	t, err := template.ParseFiles("app/views/templates/top.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// 第二引数
+	// Executeメソッドの第二引数には、テンプレートに埋め込むデータを渡す
+	// ここでは、文字列"Hello, World"を渡している
+	// テンプレートファイルには、{{.}}という記述があり、これはExecuteメソッドの第二引数で渡したデータを埋め込む場所を示している
+	err = t.Execute(w, "Hello")
+	if err != nil {
+        log.Fatal(err)
+    }
 }
