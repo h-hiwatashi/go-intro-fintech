aaf37ac28d82b075b4d4b0a71f37671927609025
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-11-26 00:33:56 +0900

    log更新

--- todo_app/app/controllers/server.go
+++ todo_app/app/controllers/server.go
@@ -10,6 +10,7 @@ func StartMAinServer()error{
 	// 第二引数にnilを渡すことで、デフォルトのマルチプレクサを使用してHTTPリクエストを処理する
 	// マルチプレクサは、リクエストを適切なハンドラにルーティングするためのHTTPリクエストマルチプレクサです。
 	// デフォルトのマルチプレクサは、404
+	// 今回は、"/"にアクセスしたときにtop関数を呼び出すように設定している
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":" + config.Config.Port, nil)
 }
