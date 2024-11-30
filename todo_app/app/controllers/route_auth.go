c40c8535b5356d1120ec379c0ef0d3ebf66c0390
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-11-30 23:21:10 +0900

    add signup form

--- todo_app/app/controllers/route_auth.go
+++ todo_app/app/controllers/route_auth.go
@@ -1,8 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo_app/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	}else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			// signup.htmlのname属性を取得
+			Name: r.PostFormValue("name"),
+			// signup.htmlのemail属性を取得
+			Email: r.PostFormValue("email"),
+			// signup.htmlのpassword属性を取得
+			Password: r.PostFormValue("password"),
+		}
 
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+		// ユーザーが作成されたら、ログインページにリダイレクト
+		http.Redirect(w, r, "/", 302)
+
+	}
 }
