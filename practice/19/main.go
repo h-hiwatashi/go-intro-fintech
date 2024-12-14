555563896e46399888454f38003e82a3751c2cb8
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-12-14 21:47:11 +0900

    generics typesats

--- practice/19/main.go
+++ practice/19/main.go
@@ -25,9 +25,22 @@ func (i MyInt) String() string {
 	return strconv.Itoa(int(i))
 }
 
+type Number interface {
+	int | int32 | int64 | float32 | float64
+}
+
+func Max[T Number](a, b T) T {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	PrintSlice([]int{1, 2, 3})
 	PrintSlice([]string{"a", "b", "c"})
 	PrintSlice([]float64{1.1, 2.2, 3.3})
 	fmt.Println(f([]MyInt{1, 2, 3}))
+	fmt.Println(Max[int](1, 2))
+	fmt.Println(Max(1.1, 2.2))
 }
